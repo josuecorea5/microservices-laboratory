afc1f8c9de36fd1670f454ed568213955970503b
paizcode <josuepaiz487@gmail.com>
2023-04-29 09:26:59 -0600

    Adding second endpoint

--- microservices/races-service/routes/service/index.js
+++ microservices/races-service/routes/service/index.js
@@ -63,26 +63,50 @@ router.get('/:race', async(req, res) => {
   const { race } = req.params;
   try {
     const dogs = await axios.get(`http://dogs:3000/api/v1/dogs/${race}`);
-    const dogsResponse = dogs.data.data;
-    const dogsAwards = dogsResponse.map((dog) => {
-      return axios.get(`http://awards:5000/api/v1/awards/dog/${dog.Id}`);
-    });
-    const dogsAwardsResponse = await Promise.all(dogsAwards);
-    const dogsAwardsData = dogsAwardsResponse.reduce((acc,dog) => {
-      return [...acc, ...dog.data.data]
-    }, []);
-    const response = {
-      data: {
-        dogs: dogsResponse,
-        awards: dogsAwardsData
-      }
-    };
 
+    const averageRace = dogs.data.data.reduce((acc,dog) => {
+      console.log(acc)
+      return {
+        ...acc,
+        race: dog.raza,
+        average: Math.ceil((acc.average + dog.peso) / 2 )
+      }
+    }, {average: 0});
+    const response = {
+      service: 'races',
+      architecture: 'microservices',
+      data: averageRace
+    };
     return res.send(response);
   } catch (error) {
     return res.status(500).send({message: 'Internal server error'})
   }
 });
+
+// router.get('/:race', async(req, res) => {
+//   const { race } = req.params;
+//   try {
+//     const dogs = await axios.get(`http://dogs:3000/api/v1/dogs/${race}`);
+//     const dogsResponse = dogs.data.data;
+//     const dogsAwards = dogsResponse.map((dog) => {
+//       return axios.get(`http://awards:5000/api/v1/awards/dog/${dog.Id}`);
+//     });
+//     const dogsAwardsResponse = await Promise.all(dogsAwards);
+//     const dogsAwardsData = dogsAwardsResponse.reduce((acc,dog) => {
+//       return [...acc, ...dog.data.data]
+//     }, []);
+//     const response = {
+//       data: {
+//         dogs: dogsResponse,
+//         awards: dogsAwardsData
+//       }
+//     };
+
+//     return res.send(response);
+//   } catch (error) {
+//     return res.status(500).send({message: 'Internal server error'})
+//   }
+// });
 
 
 module.exports = router;
